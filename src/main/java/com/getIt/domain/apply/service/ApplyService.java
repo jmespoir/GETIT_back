2763c54e2d9a40ec087f7f9e5c5e26b9b64cc24b
Y-Jimin <jak2899@naver.com>
2026-03-05 16:55:41 +0900

    feat:임시저장 불러오기 serivce 구조 작성

--- src/main/java/com/getIt/domain/apply/service/ApplyService.java
+++ src/main/java/com/getIt/domain/apply/service/ApplyService.java
@@ -1,5 +1,6 @@
 package com.getit.domain.apply.service;
 
+import com.getit.domain.apply.dto.ApplyDraftDataDto;
 import com.getit.domain.apply.dto.ApplyRequest;
 import com.getit.domain.apply.entity.Application;
 import com.getit.domain.apply.repository.ApplicationRepository;
@@ -8,6 +9,8 @@ import com.getit.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -62,5 +65,20 @@ public class ApplyService {
                             applicationRepository.save(application);
                         }
                 );
+    }
+
+    public Optional<ApplyDraftDataDto> getDraft(Long memberId) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
+
+        return applicationRepository.findFirstByMemberAndIsDraftTrue(member)
+                .map(draft -> ApplyDraftDataDto.builder()
+                        .answer1(draft.getAnswer1())
+                        .answer2(draft.getAnswer2())
+                        .answer3(draft.getAnswer3())
+                        .answer4(draft.getAnswer4())
+                        .answer5(draft.getAnswer5())
+                        .agree(false)
+                        .build());
     }
 }
