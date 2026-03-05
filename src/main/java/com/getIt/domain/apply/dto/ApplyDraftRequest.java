e8b21b8a8aac5627c18252309d3538deebfe8d8d
Y-Jimin <jak2899@naver.com>
2026-03-05 20:53:30 +0900

    fix:임시저장용 dto 분리

---
+++ src/main/java/com/getIt/domain/apply/dto/ApplyDraftRequest.java
@@ -0,0 +1,22 @@
+package com.getit.domain.apply.dto;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 임시저장 전용 요청 DTO. 미완성 상태 저장을 위해 검증 없음(필드 선택 입력 가능).
+ */
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ApplyDraftRequest {
+
+    private String answer1;
+    private String answer2;
+    private String answer3;
+    private String answer4;
+    private String answer5;
+    private Boolean agree;
+}
